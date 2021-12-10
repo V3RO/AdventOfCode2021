6eea6a32c5c0d5a760b61dd018d5ddce609cbe07
Mario Schober <30479987+V3RO@users.noreply.github.com>
2021-12-10 10:10:17 +0100

    Day 10: ez pz

--- README.md
+++ README.md
@@ -3,29 +3,29 @@ This repository contains my solution for the [Advent of Code](https://adventofco
 The Problems are solved with C# (NET5). <br>
 The solutions are probably often not solved optimally, because I usually implement my first idea instead of looking for the best/most optimal solution.
 
-| Day 	| Challenge 	                                        |
-|-----	|-----------	                                        |               
-| 1   	| [Sonar Sweep](./SonarSweep)                         |
-| 2   	| [Dive!](./Dive)                                     |
-| 3   	| [Binary Diagnostic](./BinaryDiagnostic)             |
-| 4   	| [Giant Squid](./GiantSquid)                         |
-| 5   	| [Hydrothermal Venture](./HydrothermalVenture)       |
-| 6   	| [Lanternfish](./LanternFish)                        |
-| 7   	| [The Treachery of Whales](./TheTreacheryOfWhales)   |
-| 8   	| [Seven Segment Search](./SevenSegmentSearch)        |
-| 9   	| [Smoke Basin](./SmokeBasin)                         |
-| 10  	|           	|
-| 11  	|           	|
-| 12  	|           	|
-| 13  	|           	|
-| 14  	|           	|
-| 15  	|           	|
-| 16  	|           	|
-| 17  	|           	|
-| 18  	|           	|
-| 19  	|           	|
-| 20  	|           	|
-| 21  	|           	|
-| 22  	|           	|
-| 23  	|           	|
-| 24  	|           	|
+| Day 	| Challenge 	                                       |
+|-----	|---------------------------------------------------|               
+| 1   	| [Sonar Sweep](./SonarSweep)                       |
+| 2   	| [Dive!](./Dive)                                   |
+| 3   	| [Binary Diagnostic](./BinaryDiagnostic)           |
+| 4   	| [Giant Squid](./GiantSquid)                       |
+| 5   	| [Hydrothermal Venture](./HydrothermalVenture)     |
+| 6   	| [Lanternfish](./LanternFish)                      |
+| 7   	| [The Treachery of Whales](./TheTreacheryOfWhales) |
+| 8   	| [Seven Segment Search](./SevenSegmentSearch)      |
+| 9   	| [Smoke Basin](./SmokeBasin)                       |
+| 10  	| [Syntax Scoring](./SyntaxScoring)          	      |
+| 11  	| 	                                                 |
+| 12  	| 	                                                 |
+| 13  	| 	                                                 |
+| 14  	| 	                                                 |
+| 15  	| 	                                                 |
+| 16  	| 	                                                 |
+| 17  	| 	                                                 |
+| 18  	| 	                                                 |
+| 19  	| 	                                                 |
+| 20  	| 	                                                 |
+| 21  	| 	                                                 |
+| 22  	| 	                                                 |
+| 23  	| 	                                                 |
+| 24  	| 	                                                 |
---
+++ SyntaxScoring/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SyntaxScoring
+{
+    internal static class Program
+    {
+        private static List<string> ReadInputData()
+        {
+            return File.ReadLines($"{Directory.GetCurrentDirectory()}/input.txt").ToList();
+        }
+
+        private static void PartOne()
+        {
+            var inputData = ReadInputData();
+            var errorSum = 0;
+            
+            foreach (var line in inputData)
+            {
+                var chunks = new Stack<char>();
+
+                foreach (var c in line)
+                {
+                    if (c is '(' or '{' or '[' or '<')
+                    {
+                        chunks.Push(c);
+                    }
+                    else
+                    {
+                        var opening = chunks.Pop();
+                        if (opening is '(' && c is ')' ||
+                            opening is '[' && c is ']' ||
+                            opening is '{' && c is '}' ||
+                            opening is '<' && c is '>') continue;
+                        switch (c)
+                        {
+                            case ')':
+                                errorSum += 3;
+                                break;
+                            case ']':
+                                errorSum += 57;
+                                break;
+                            case '}':
+                                errorSum += 1197;
+                                break;
+                            case '>':
+                                errorSum += 25137;
+                                break;
+                        }
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(errorSum);
+        }
+        
+        private static void PartTwo()
+        {
+            var inputData = ReadInputData();
+            var completionScores = new List<long>();
+            
+            foreach (var line in inputData)
+            {
+                var chunks = new Stack<char>();
+                var corrupted = false;
+
+                foreach (var c in line)
+                {
+                    if (c is '(' or '{' or '[' or '<')
+                    {
+                        chunks.Push(c);
+                    }
+                    else
+                    {
+                        var opening = chunks.Pop();
+                        if (opening is '(' && c is not ')' ||
+                            opening is '[' && c is not ']' ||
+                            opening is '{' && c is not '}' ||
+                            opening is '<' && c is not '>') corrupted = true;
+                    }
+                }
+
+                if (corrupted)
+                    continue;
+                
+                long completionScore = 0;
+                while (chunks.Count > 0)
+                {
+                    var opening = chunks.Pop();
+                    completionScore = opening switch
+                    {
+                        '(' => completionScore * 5 + 1,
+                        '[' => completionScore * 5 + 2,
+                        '{' => completionScore * 5 + 3,
+                        '<' => completionScore * 5 + 4,
+                        _ => completionScore
+                    };
+                }
+                completionScores.Add(completionScore);
+            }
+
+            Console.WriteLine(completionScores.OrderByDescending(i => i).Skip(completionScores.Count / 2).Take(1).First());
+        }
+        
+        private static void Main(string[] args)
+        {
+            PartOne();
+            PartTwo();
+        }
+    }
+}
